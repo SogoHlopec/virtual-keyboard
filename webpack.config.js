0689db90b0b49150fb0f1022b6b3fbbeeca8f3c8
SogoHlopec <sogo246@gmail.com>
2023-04-28 23:22:38 +0300

    feat: add basic configs

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const devtool = "source-map";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  devtool,
+  entry: {
+    index: path.resolve(__dirname, "src", "index.js"),
+    ["pets"]: path.resolve(__dirname, "src", "pages", "pets", "index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    filename: "[name].js",
+    assetModuleFilename: path.join("[name].[contenthash][ext]"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: path.join("[name].[ext]"),
+        },
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/resource",
+        generator: {
+          filename: path.join("[name].[ext]"),
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "pages", "pets", "index.html"),
+      filename: "pets.html",
+      chunks: ["pets"],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
